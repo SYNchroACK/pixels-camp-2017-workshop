101a65e898b7904cb085c7667ee6357e1830086b
Renato Cardoso <renato.cardoso@paddypowerbetfair.com>
2017-09-28 15:20:39 +0100

    03 - web app offline mode

---
+++ client/public/sw.js
@@ -0,0 +1,26 @@
+const cacheVersion = 'PixelsCampTodoApp';
+const appCachedResources = [
+    '/',
+    '/manifest.json',
+    '/android-icon-96x96.png',
+    '/android-icon-144x144.png',
+    '/android-icon-192x192.png',
+    '/static/js/bundle.js',
+    '/static/media/heroMoon.bb1442b6.jpg',
+    '/static/media/pixelsCamp.9a750d05.svg'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheVersion).then(cache => {
+            cache.addAll(appCachedResources);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
